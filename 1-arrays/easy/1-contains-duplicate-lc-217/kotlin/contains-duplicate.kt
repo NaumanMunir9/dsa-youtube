26a7ef8bbebbfc95878ec64bb556d66256c527b9
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2024-12-31 17:56:43 +0500

    kt

---
+++ 1-arrays/easy/1-contains-duplicate-lc-217/kotlin/contains-duplicate.kt
@@ -0,0 +1,12 @@
+import java.util.HashSet
+
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        val hashSet = HashSet<Int>()
+
+        for (num in nums) {
+            if (!hashSet.add(num)) return true
+        }
+        return false
+    }
+}
