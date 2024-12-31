3be8b20720f3df53f0f28521d1e3c027fff08c27
M. Nauman Munir <41162751+NaumanMunir9@users.noreply.github.com>
2024-12-31 17:33:13 +0500

    js

---
+++ 1-arrays/easy/1-contains-duplicate-lc-217/js/contains-duplicate.js
@@ -0,0 +1,12 @@
+var containsDuplicate = (nums) => {
+    const set = new Set();
+
+    for (const num of nums) {
+        if (set.has(num)) {
+            return true;
+        } else {
+            set.add(num);
+        }
+    }
+    return false;
+};
